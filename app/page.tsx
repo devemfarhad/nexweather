c6f4e20e6eb8584cf6c9d4cefb642c494fc57d41
devemfarhad <dev.emranfarhad@gmail.com>
2023-04-23 01:06:13 +0600

    Initial setup done

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <main className="text-6xl">
+      <h1>Let's make a GPT- app</h1>
+    </main>
+  )
+}
