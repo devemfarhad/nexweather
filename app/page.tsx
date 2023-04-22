0731915bb2f02d13db72cffa228d7205eea0201f
devemfarhad <dev.emranfarhad@gmail.com>
2023-04-23 01:51:43 +0600

    Built Hero section for city pick without the city picker

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,24 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>Let's make a GPT- app</h1>
-    </main>
+    
+    <div className="min-h-screen bg-gradient-to-br from-[#1378fd] to-[#3b63e6] p-10 flex flex-col justify-center">
+        
+        {/*Building the Hero section*/}
+
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-4xl font-bold text-center md-10">
+          NexsWeather AI
+        </Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 Plus More Awesomeness
+        </Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#FDB813] to-[#e48e2b]">
+          {/*CityPicker*/}
+        </Card>
+      </Card>
+      
+    </div>
   )
 }
